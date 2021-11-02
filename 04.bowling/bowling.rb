fb7843c97dc11fabc0ce0dc5b65783adac5758e7
kino <himekiyoto@gmail.com>
2021-11-02 23:34:59 +0900

    Change the name of variables and how to add elements to frames(array)

--- 04.bowling/bowling.rb
+++ 04.bowling/bowling.rb
@@ -11,42 +11,32 @@ scores.each do |s|
            end
 end
 
-@frames = []
-arrays = shots.slice_after { |n| n == 10 }.to_a
-arrays.each do |arr|
+frames = []
+arrays_sliced_after_strike = shots.slice_after { |n| n == 10 }.to_a
+arrays_sliced_after_strike.each do |arr|
   arr.each_slice(2) do |s|
-    @frames << s
+    frames << s
   end
 end
 
-if @frames[10] && @frames[11]
-  @frames[9].concat(@frames[10], @frames[11])
-  @frames.delete_at(11)
-  @frames.delete_at(10)
-elsif @frames[10]
-  @frames[9].concat(@frames[10])
-  @frames.delete_at(10)
-end
+frames << frames.slice!(9..).flatten
 
 point = 0
-@frames.each_with_index do |frame, index|
+frames.each_with_index do |frame, index|
   strike = frame[0] == 10
-  third_throw = frame[2]
-  sum = frame.sum
-  next_frame = @frames[index + 1]
-  frame_after_next = @frames[index + 2]
-  point += if strike && third_throw
-             sum
-           elsif strike && !next_frame[1]
-             sum + next_frame[0] + frame_after_next[0]
+  frame_sum = frame.sum
+  next_frame = frames[index + 1]
+  frame_after_next = frames[index + 2]
+  point += if frame.size == 3
+             frame_sum
+           elsif strike && next_frame.size == 1
+             frame_sum + next_frame[0] + frame_after_next[0]
            elsif strike
-             sum + next_frame[0] + next_frame[1]
-           elsif sum >= 10 && third_throw
-             sum
-           elsif sum == 10
-             sum + next_frame[0]
+             frame_sum + next_frame[0] + next_frame[1]
+           elsif frame_sum == 10
+             frame_sum + next_frame[0]
            else
-             sum
+             frame_sum
            end
 end
 puts point
