6f866248bd72e842d4e73e387d9e53d35989e818
kino <himekiyoto@gmail.com>
2021-11-12 18:33:30 +0900

    Add ls with option a

--- 05.ls/ls.rb
+++ 05.ls/ls.rb
@@ -1,5 +1,15 @@
 # frozen_string_literal: true
 
+require 'optparse'
+
+NUM_OF_ROWS = 3
+
+def receive_option(params)
+  opt = OptionParser.new
+  opt.on('-a', '--all', 'Include directory entries whose names begin with a dot') { |v| params[:a] = v }
+  opt.parse!(ARGV)
+end
+
 def adjust_digits(columns)
   columns.each do |column|
     column.map! do |dir_or_file|
@@ -15,10 +25,16 @@ def display_columns(columns)
   end
 end
 
-NUM_OF_ROWS = 3
-
 def ls
-  dirs_and_files = Dir.glob('*')
+  params = {}
+  gain_option(params)
+
+  dirs_and_files = if params[:a]
+                     Dir.glob('*', File::FNM_DOTMATCH)
+                   else
+                     Dir.glob('*')
+                   end
+
   unless (dirs_and_files.size % NUM_OF_ROWS).zero?
     (NUM_OF_ROWS - dirs_and_files.size % NUM_OF_ROWS).times do
       dirs_and_files << ''
