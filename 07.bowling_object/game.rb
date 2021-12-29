f7afe50b3434228c982df4b8c884199b3a3a0230
kino <himekiyoto@gmail.com>
2021-12-29 11:42:03 +0900

    Use Array#sum instead of a variable: total

--- 07.bowling_object/game.rb
+++ 07.bowling_object/game.rb
@@ -15,31 +15,27 @@ class Game
       frame << mark
       # (9フレーム目まで) && (frameが既に2投分の情報を持つ || ストライクのとき)
       if (frames.count < 9) && ((frame.count == 2) || (frame[0] == 'X'))
-        frames << frame
+        frames << Frame.new(*frame)
         frame = []
       end
     end
     # 10フレーム目を格納
-    frames << frame
+    frames << Frame.new(*frame)
   end
 
   def calculate_score
-    total = 0
-    @frames.each_with_index do |f, i|
-      frame = Frame.new(*f)
-      next_frame = Frame.new(*@frames[i + 1]) if i < 9
+    @frames.each_with_index.sum do |frame, i|
+      next_frame = @frames[i + 1] if i < 9
 
       if frame.strike? && next_frame.strike?
-        frame_after_next = Frame.new(*@frames[i + 2])
-        total += (frame.score + next_frame.first_shot.score + frame_after_next.first_shot.score)
+        frame.score + next_frame.first_shot.score + @frames[i + 2].first_shot.score
       elsif frame.strike?
-        total += (frame.score + next_frame.first_shot.score + next_frame.second_shot.score)
+        frame.score + next_frame.first_shot.score + next_frame.second_shot.score
       elsif frame.spare?
-        total += (frame.score + next_frame.first_shot.score)
+        frame.score + next_frame.first_shot.score
       else
-        total += frame.score
+        frame.score
       end
     end
-    total
   end
 end
